f4583bdf0fb4b02a5773b5c77003853da9d53143
Natasha <natashamwangi1@gmail.com>
2025-04-04 22:34:58 +0300

    Portfolio Hackathon

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-up');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.1 });
+
+  document.querySelectorAll('section').forEach(section => {
+    observer.observe(section);
